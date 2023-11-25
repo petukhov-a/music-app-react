c0a43112387dd733e04968e515c8cff287f39e41
Anton <reopik@mail.ru>
2023-11-25 15:41:09 +0300

    created AudioContex. Made playing and pause tracks

--- src/components/Track.tsx
+++ src/components/Track.tsx
@@ -1,19 +1,31 @@
-import React, { FC } from 'react';
+import { FC, useContext } from 'react';
 import { TrackItem } from '../assets/tracklist';
 import style from "./track.module.scss";
 import { IconButton } from '@mui/material';
-import { PlayArrow } from '@mui/icons-material';
+import { PlayArrow, Pause } from '@mui/icons-material';
 import secondsToMMSS from '../utils/secondsToMMSS';
+import { AudioContext } from '../context/AudioContext';
+import cn from 'classnames';
 
 const Track: FC<TrackItem> = (track) => {
   const { id, src, preview, title, artists, duration } = track;
 
+  const audioContext = useContext(AudioContext);
+
+  if (!audioContext) {
+    return null;
+  }
+
+  const { handleToggleAudio, currentTrack, isPlaying } = audioContext;
+
+  const isCurrentTrack = currentTrack.id === track.id;
+
   const formattedDuration = secondsToMMSS(duration);
 
   return (
-    <div className={style.track}>
-      <IconButton>
-        <PlayArrow />
+    <div className={cn(style.track, isCurrentTrack && style.playing)}>
+      <IconButton onClick={() => handleToggleAudio(track)}>
+        {isPlaying && isCurrentTrack ? <Pause /> : <PlayArrow />}
       </IconButton>
       <img className={style.preview} src={preview} alt='' />
       <div className={style.credits}>
---
+++ src/context/AudioContext.tsx
@@ -0,0 +1,52 @@
+import { FC, ReactNode, createContext, useState } from "react"
+import { TrackItem, tracklist } from "../assets/tracklist";
+
+type AudioContextType = {
+  currentTrack: TrackItem;
+  isPlaying: boolean;
+  handleToggleAudio: (track: TrackItem) => void;
+}
+
+type AudioProviderProps = {
+  children: ReactNode;
+}
+
+const audio = new Audio();
+
+export const AudioContext = createContext<AudioContextType | null>(null);
+
+const AudioProvider: FC<AudioProviderProps> = ({ children }) => {
+  const [currentTrack, setCurrentTrack] = useState(tracklist[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handleToggleAudio = (track: TrackItem) => {
+    if (currentTrack.id !== track.id) {
+      setCurrentTrack(track);
+      setIsPlaying(true);
+
+      audio.src = track.src;
+      audio.currentTime = 0;
+      audio.play();
+
+      return;
+    }
+
+    if (isPlaying) {
+      audio.pause();
+      setIsPlaying(false);
+    } else {
+      audio.play();
+      setIsPlaying(true);
+    }
+  }
+
+  const value = { currentTrack, isPlaying, handleToggleAudio }
+
+  return (
+    <AudioContext.Provider value={value}>
+      {children}
+    </AudioContext.Provider>
+  )
+}
+
+export default AudioProvider;
